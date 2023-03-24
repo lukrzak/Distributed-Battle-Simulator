314a23c1a80bc17dc65209a4dac7d801ca7aa304
lukrzak <lukrzak@wp.pl>
2023-03-24 16:44:52 +0100

    Implemented attack method. Moved values of counterDamageFactor to constructor

--- Server/src/main/java/com/dbs/dbs/services/UnitService.java
+++ Server/src/main/java/com/dbs/dbs/services/UnitService.java
@@ -9,31 +9,25 @@ import java.util.HashMap;
 
 @Service
 public class UnitService {
-    private final HashMap<Pair<Class<?>, Class<?>>, Double> counterFactor = new HashMap<>();
+    private final HashMap<Pair<Class<? extends Unit>, Class<? extends Unit>>, Double> counterDamageFactor = new HashMap<>();
 
     public UnitService() {
-        counterFactor.put(new ImmutablePair<>(Pikeman.class, Knight.class), 1.5);
-        counterFactor.put(new ImmutablePair<>(Archer.class, Footman.class), 1.1);
-        counterFactor.put(new ImmutablePair<>(HeavyFootman.class, Pikeman.class), 1.2);
-        counterFactor.put(new ImmutablePair<>(Knight.class, Archer.class), 1.3);
-
-        //terrainFactor.put(new ImmutablePair<>())
+        counterDamageFactor.put(new ImmutablePair<>(Pikeman.class, Knight.class), 1.5);
+        counterDamageFactor.put(new ImmutablePair<>(Archer.class, Footman.class), 1.1);
+        counterDamageFactor.put(new ImmutablePair<>(HeavyFootman.class, Pikeman.class), 1.2);
+        counterDamageFactor.put(new ImmutablePair<>(Knight.class, Archer.class), 1.3);
     }
 
-    public Double getCounterFactor(Class<?> attacker, Class<?> defender){
-        return counterFactor.get(new ImmutablePair<>(attacker, defender)) == null ?
-                1 : counterFactor.get(new ImmutablePair<>(attacker, defender));
-    }
-
-    public Integer getTerrainFactor(Class<?> unit, Class<?> terrain){
-        return 0;
+    public Double getCounterFactor(Class<? extends Unit> attacker, Class<? extends Unit> defender){
+        return counterDamageFactor.get(new ImmutablePair<>(attacker, defender)) == null ?
+                1 : counterDamageFactor.get(new ImmutablePair<>(attacker, defender));
     }
 
     public void attack(Unit attacker, Unit defender){
-
+        double damageDealt = attacker.getDamage() * getCounterFactor(attacker.getClass(), defender.getClass());
+        defender.setHealth(defender.getHealth() - damageDealt);
     }
 
     public void useSpecialAbility(Unit unit){
-
     }
 }
