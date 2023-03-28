658ed97512a7bedbeba0b84a536ea3f089dce25f
lukrzak <lukrzak@wp.pl>
2023-03-28 22:32:34 +0200

    Created Game class

---
+++ Server/src/main/java/com/dbs/dbs/models/Game.java
@@ -0,0 +1,7 @@
+package com.dbs.dbs.models;
+
+public class Game {
+
+    public Game() {
+    }
+}
---
+++ Server/src/test/java/com/dbs/dbs/GameTests.java
@@ -0,0 +1,16 @@
+package com.dbs.dbs;
+
+import com.dbs.dbs.services.GameService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameTests {
+
+    GameService game = new GameService();
+
+    @Test
+    void getMovingDirectionAngleTest(){
+
+    }
+}
