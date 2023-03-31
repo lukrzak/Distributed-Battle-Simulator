9bcf37d0e0c78dcf33594d3043dcd4357c655eeb
lukrzak <lukrzak@wp.pl>
2023-03-31 20:13:26 +0200

    Created simulation scenario with attacking and running at the same time

--- Server/src/test/java/com/dbs/dbs/GameTests.java
+++ Server/src/test/java/com/dbs/dbs/GameTests.java
@@ -1,12 +1,12 @@
 package com.dbs.dbs;
 
-import com.dbs.dbs.models.units.Footman;
-import com.dbs.dbs.models.units.Unit;
+import com.dbs.dbs.controllers.GameController;
+import com.dbs.dbs.enumerations.UnitEnum;
+import com.dbs.dbs.models.units.*;
 import com.dbs.dbs.services.GameService;
+import com.dbs.dbs.services.UnitService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -15,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class GameTests {
 
-    GameService gameService = new GameService();
+    GameService gameService = new GameService(new UnitService());
+    GameController gameController = new GameController(gameService);
 
     @Test
     void getMovingDirectionAngleTest(){
@@ -33,5 +34,26 @@ public class GameTests {
         assertTrue(sin(gameService.getMovingDirectionAngle(unit, -1.0, -1.0)) < 0);
     }
 
+    @Test
+    void attackAndRunSimulation() throws InterruptedException {
+        Unit attacker = UnitFactory.createUnit(UnitEnum.FOOTMAN, 0.0, 0.0);
+        Unit defender = UnitFactory.createUnit(UnitEnum.PIKEMAN, 0.0, 1.0);
 
+        System.out.println(attacker.getName() + " initiated fight with " + defender.getName());
+        System.out.println("--------------------");
+        gameController.attackUnit(attacker, defender);
+
+        Thread.sleep(2000);
+        System.out.println("--------------------");
+        System.out.println(defender.getName() + " decided to flee after 2 seconds!");
+        System.out.println("--------------------");
+        gameController.moveUnit(defender, 10.0, 10.0);
+
+        System.out.println("--------------------");
+        System.out.println(attacker.getName() + " started running after " + defender.getName());
+        System.out.println("--------------------");
+        gameController.moveUnit(attacker, 10.0, 10.0);
+
+        Thread.sleep(10000);
+    }
 }
