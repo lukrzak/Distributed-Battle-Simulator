eb84a8b9d0e580846f738e6f6a6b1756e65036c7
lukrzak <lukrzak@wp.pl>
2023-03-31 20:14:01 +0200

    Created attack method in new thread invoked in controller

--- Server/src/main/java/com/dbs/dbs/controllers/GameController.java
+++ Server/src/main/java/com/dbs/dbs/controllers/GameController.java
@@ -6,24 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+/**
+ * GameController is WebSocket controller, that receives and manages incoming messages.
+ */
 @Controller
 public class GameController{
 
+    /**
+     * Instance of GameService.
+     */
     private final GameService gameService;
 
+    /**
+     * Constructor of GameController.
+     * @param gameService Autowired GameService instance.
+     */
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
 
+    /**
+     * Method for connection test. After successful invocation, displays current time.
+     */
     @MessageMapping("/test")
     public void test(){
-        System.out.println("Test");
+        System.out.println(java.time.LocalTime.now());
     }
 
+    /**
+     * moveUnit creates thread, where unit is moved towards point (posX, posY).
+     * @param unit Object of Unit type, that will be moved.
+     * @param posX X coordinate of destination.
+     * @param posY Y coordinate of destination.
+     */
     @MessageMapping("/move")
     public void moveUnit(Unit unit, double posX, double posY){
-        Thread newThread = new Thread(new Runnable() {
+        Thread moveThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -33,7 +52,27 @@ public class GameController{
                 }
             }
         });
-        newThread.start();
+        moveThread.start();
     }
 
+    /**
+     * attackUnit is responsible for attacking unit in time intervals.
+     * Method creates new Thread, that ends when defender is out of range.
+     * @param attacker Object of Unit type. This unit will deal damage toward defender.
+     * @param defender Object of Unit type. This unit will receive dealt damage.
+     */
+    @MessageMapping("/attack")
+    public void attackUnit(Unit attacker, Unit defender){
+        Thread attackThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    gameService.attackUnit(attacker, defender);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        attackThread.start();
+    }
 }
--- Server/src/main/java/com/dbs/dbs/services/GameService.java
+++ Server/src/main/java/com/dbs/dbs/services/GameService.java
@@ -1,16 +1,18 @@
 package com.dbs.dbs.services;
 
 import com.dbs.dbs.models.units.Unit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
 
 import static java.lang.Math.*;
 
 @Service
 public class GameService{
 
-    public GameService() {
+    @Autowired
+    private final UnitService unitService;
+    public GameService(UnitService unitService) {
+        this.unitService = unitService;
     }
 
     public void moveUnit(Unit unit, Double newX, Double newY) throws InterruptedException {
@@ -36,6 +38,24 @@ public class GameService{
 
     public double getMovingDirectionAngle(Unit unit, Double newX, Double newY){
         return Math.atan2((newY - unit.getPositionY()), (newX - unit.getPositionX()));
+    }
+
+    public void attackUnit(Unit attacker, Unit defender) throws InterruptedException {
+        double distance = sqrt(pow(attacker.getPositionX() - defender.getPositionX(), 2)
+                + pow(attacker.getPositionY() - defender.getPositionY(), 2));
+        double damageFactor = unitService.getCounterFactor(attacker.getClass(), defender.getClass());
+
+        while(distance <= attacker.getRange()){
+            double damage = attacker.getDamage() * damageFactor;
+            defender.setHealth(defender.getHealth() - damage);
+            distance = sqrt(pow(attacker.getPositionX() - defender.getPositionX(), 2)
+                    + pow(attacker.getPositionY() - defender.getPositionY(), 2));
+
+            System.out.println(attacker.getName() + " attacked " + defender.getName() + " for " + damage + " damage");
+            System.out.println("Defender " + defender.getName() + " has now " + defender.getHealth() + " health");
+            Thread.sleep(500);
+        }
+        System.out.println("Out of range");
     }
 
 }
