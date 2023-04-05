d1e3b6d0a1dc11afcfd7c91516cacf6ea09e6500
lukrzak <lukrzak@wp.pl>
2023-04-05 13:55:01 +0200

    Created websocket connection on its own thread

--- Client/test/UnitTests.py
+++ Client/test/UnitTests.py
@@ -1,5 +1,9 @@
+import json
+import threading
 import websocket
+import tkinter as tk
 
+ws = None
 
 def on_message(ws, message):
     print(message)
@@ -15,13 +19,32 @@ def on_close(ws):
 
 def on_open(ws):
     print("WebSocket opened")
-    ws.send("Hello, server!")
 
 
-if __name__ == "__main__":
+def send_message():
+    data = {
+        "name": "button_press",
+        "value": "pressed"
+    }
+    ws.send(json.dumps(data))
+
+
+def start_websocket():
+    global ws
     ws = websocket.WebSocketApp("ws://localhost:8080/app",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
     ws.run_forever()
+
+
+t = threading.Thread(target=start_websocket)
+t.daemon = True
+t.start()
+
+root = tk.Tk()
+button = tk.Button(root, text="Send message", command=send_message)
+button.pack()
+
+root.mainloop()
